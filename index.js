752dd460d8c1c9c44dcb77e9f5c566b3f1070c95
Uwe Wiemer <hallowatcher@gmail.com>
2016-12-14 23:06:40 +0100

    Changed check for update file

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ const updater = {
         let localAsar = appPath;
         let updateAsar = this.update.file;
 
-        if (!fs.existsSync(updateAsar)) return this.end('Update file does not exist at ' + updateAsar);
+        if (updateAsar) return this.end('Update file does not exist!');
 
         let winArgs = `${updaterPath} ${updateAsar} ${localAsar}`;
         if (process.platform === 'win32') {
