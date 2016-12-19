135117e480e447ec535384c204f2b7b50428a6ac
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2016-12-19 10:35:48 +0100

    Changed to observable pattern

--- index.js
+++ index.js
@@ -4,120 +4,164 @@ const path = require('path');
 const semver = require('semver');
 const fs = require('fs');
 const request = require('request');
+const EventEmitter = require('events').EventEmitter;
 
 const child = require('child_process');
 
-const updater = {
+function Updater(config) {
+    EventEmitter.call(this);
 
-    // The callback to run after checking
-    callback: null,
-
-    // The configuration
-    config: {
+    var defaults = {
         repository: null,
         version: null,
         downloadPath: null
-    },
+    };
 
-    // The update retrieved from github
-    update: {
-        version: null,
-        source: null,
-        file: null
-    },
+    this.config = util._extend(defaults, config);
+}
+util.inherits(Updater, EventEmitter);
 
-    // Initialize
-    init: function (config) {
-        this.config = util._extend(this.config, config);
-    },
+Updater.prototype.checkForUpdates = function () {
+    request.get(`https://api.github.com/repos/${this.config.repository}/releases`, (error, response, body) => {
+        if (error) return this.emit('error', new Error('Could not connect to repo!'));
+        
+        try {
+            var data = JSON.parse(body);
 
-    // Check for an update
-    check: function (callback) {
-        if (callback) this.callback = callback;
+            if (!data) throw new Error('No data was received!');
+            if (data.length === 0) throw new Error('No releases have been made!');
+            if (!semver.valid(this.config.version)) throw new Error('The version specified is invalid!');
+            if (!semver.valid(data[0].tag_name)) throw new Error('The latest release in github has an invalid tag version!');
 
-        if (!this.config.repository)
-            return this.end('Please specify the repo in the config!')
+            let localVer = semver.clean(this.config.version);
+            let latestVer = semver.clean(data[0].tag_name);
 
-        if (!this.config.version)
-            return this.end('Please specify the version in the config!');
+            if (semver.gt(latestVer, localVer)) {
+                if (!data[0].assets) throw new Error('The latest version has no assets!');
+                let asset = data[0].assets.find(asset => asset.name.endsWith('.exe'));
+                if (!asset) throw new Error('The latest version has no asset that ends with ".exe"');
 
-        request.get(`https://api.github.com/repos/${this.config.repository}/releases`, (error, response, body) => {
-            if (error) return this.end('Could not connect to repo!');
-            
-            try {
-                var data = JSON.parse(body);
-
-                if (!data) throw 'No data was received!';
-                if (data.length === 0) throw 'No releases have been made!';
-                if (!semver.valid(this.config.version)) throw 'The version specified is invalid!';
-                if (!semver.valid(data[0].tag_name)) throw 'The latest release in github has an invalid tag version!';
-
-                let localVer = semver.clean(this.config.version);
-                let latestVer = semver.clean(data[0].tag_name);
-
-                if (semver.gt(latestVer, localVer)) {
-                    if (!data[0].assets) throw 'The latest version has no assets!';
-                    let asset = data[0].assets.find(asset => asset.name.endsWith('.exe'));
-                    if (!asset) throw 'The latest version has no asset that ends with ".exe"';
-
-                    this.update.version = latestVer;
-                    this.update.source = asset.browser_download_url;
-                }
-
-                return this.end();
-            } catch (e) {
-                this.end(e);
+                return this.emit('updates-found', { version: latestVer, source: asset.browser_download_url });
             }
-        });
 
-    },
-
-    // Update
-    download: function (callback) {
-        if (callback) this.callback = callback;
-
-        if (!this.update.ver || !this.update.source) return this.end('No updates in queue!');
-
-        let url = this.update.source;
-        let fileName = 'update.exe';
-
-        let updateFile = path.join(resourcesPath, fileName);
-        let updateFileStream = fs.createWriteStream(updateFile);
-        request.get(url)
-            .on('error', (error) => {
-                return this.end(error);
-            })
-            .pipe(updateFileStream)
-            .on('finish', () => {
-                this.update.file = updateFile;
-                setInterval(() => {
-                    this.end();
-                }, 2000);
-                return;
-            });
-    },
-
-    apply: function (callback) {
-        if (callback) this.callback = callback;
-
-        let updateExe = this.update.file;
-        if (!updateExe) return this.end('Update file does not exist!');
-
-        if (process.platform === 'win32') {
-            child.spawn('cmd', ['/s', '/c', `"${updateExe}"`], { detached: true, windowsVerbatimArguments: true, stdio: 'ignore' }).unref();
-            this.end();
-        } else {
-            return this.end('Only windows supported for now!');
+            return this.emit('updates-none');
+        } catch (e) {
+            this.emit('error', e);
         }
-    },
-
-    end: function (error) {
-        if (typeof this.callback !== 'function') return false;
-
-        this.callback.call(this, error, this.update.ver);
-        this.callback = null;
-    }
-
+    });
 }
 
-module.exports = updater;
+// const updater = {
+
+//     // The callback to run after checking
+//     callback: null,
+
+//     // The configuration
+//     config: {
+//         repository: null,
+//         version: null,
+//         downloadPath: null
+//     },
+
+//     // The update retrieved from github
+//     update: {
+//         version: null,
+//         source: null,
+//         file: null
+//     },
+
+//     // Initialize
+//     init: function (config) {
+//         this.config = util._extend(this.config, config);
+//     },
+
+//     // Check for an update
+//     check: function (callback) {
+//         if (callback) this.callback = callback;
+
+//         if (!this.config.repository)
+//             return this.end('Please specify the repo in the config!')
+
+//         if (!this.config.version)
+//             return this.end('Please specify the version in the config!');
+
+//         request.get(`https://api.github.com/repos/${this.config.repository}/releases`, (error, response, body) => {
+//             if (error) return this.end('Could not connect to repo!');
+            
+//             try {
+//                 var data = JSON.parse(body);
+
+//                 if (!data) throw 'No data was received!';
+//                 if (data.length === 0) throw 'No releases have been made!';
+//                 if (!semver.valid(this.config.version)) throw 'The version specified is invalid!';
+//                 if (!semver.valid(data[0].tag_name)) throw 'The latest release in github has an invalid tag version!';
+
+//                 let localVer = semver.clean(this.config.version);
+//                 let latestVer = semver.clean(data[0].tag_name);
+
+//                 if (semver.gt(latestVer, localVer)) {
+//                     if (!data[0].assets) throw 'The latest version has no assets!';
+//                     let asset = data[0].assets.find(asset => asset.name.endsWith('.exe'));
+//                     if (!asset) throw 'The latest version has no asset that ends with ".exe"';
+
+//                     this.update.version = latestVer;
+//                     this.update.source = asset.browser_download_url;
+//                 }
+
+//                 return this.end();
+//             } catch (e) {
+//                 this.end(e);
+//             }
+//         });
+
+//     },
+
+//     // Update
+//     download: function (callback) {
+//         if (callback) this.callback = callback;
+
+//         if (!this.update.ver || !this.update.source) return this.end('No updates in queue!');
+
+//         let url = this.update.source;
+//         let fileName = 'update.exe';
+
+//         let updateFile = path.join(resourcesPath, fileName);
+//         let updateFileStream = fs.createWriteStream(updateFile);
+//         request.get(url)
+//             .on('error', (error) => {
+//                 return this.end(error);
+//             })
+//             .pipe(updateFileStream)
+//             .on('finish', () => {
+//                 this.update.file = updateFile;
+//                 setInterval(() => {
+//                     this.end();
+//                 }, 2000);
+//                 return;
+//             });
+//     },
+
+//     apply: function (callback) {
+//         if (callback) this.callback = callback;
+
+//         let updateExe = this.update.file;
+//         if (!updateExe) return this.end('Update file does not exist!');
+
+//         if (process.platform === 'win32') {
+//             child.spawn('cmd', ['/s', '/c', `"${updateExe}"`], { detached: true, windowsVerbatimArguments: true, stdio: 'ignore' }).unref();
+//             this.end();
+//         } else {
+//             return this.end('Only windows supported for now!');
+//         }
+//     },
+
+//     end: function (error) {
+//         if (typeof this.callback !== 'function') return false;
+
+//         this.callback.call(this, error, this.update.ver);
+//         this.callback = null;
+//     }
+
+// }
+
+module.exports = Updater;
--- test/updater-test.js
+++ test/updater-test.js
@@ -1,116 +1,127 @@
 
-
 const assert = require('assert');
 const proxyquire =  require('proxyquire');
 const sinon = require('sinon');
 
 var requestStub = {};
-var updater = proxyquire('../index', { 'request': requestStub });
+var GithubUpdater = proxyquire('../index', { 'request': requestStub });
 
 describe('updater with valid settings', function () {
 
-    // Test settings
-    let repo = "test/repo";
-    let ver = "0.0.9";
-    let path = "./";
+    var updater, sandbox;
+
+    // Settings
+    let repository = 'test/repo';
+    let version = '0.0.9';
+    let downloadPath = './';
 
     beforeEach(function () {
-        updater.init({
-            repository: repo,
-            version: ver,
-            downloadPath: path
+        sandbox = sinon.sandbox.create();
+
+        updater = new GithubUpdater({
+            repository: repository,
+            version: version,
+            downloadPath: downloadPath
         });
+    });
+
+    afterEach(function () {
+        updater = null;
+        sandbox.restore();
     });
 
     it('should initialize the module', function () {
-        assert(updater.config.repository === repo);
-        assert(updater.config.version === ver);
-        assert(updater.config.downloadPath === path);
-
+        assert(updater.config.repository === repository);
+        assert(updater.config.version === version);
+        assert(updater.config.downloadPath === downloadPath);
     });
 
     it('should get from the right repository', function () {
-        var get = sinon.stub(requestStub, 'get', function (url, callback) {
-            assert(url.includes(repo));
+        let get = sandbox.stub(requestStub, 'get', function (url, callback) {
+            assert(url.includes(repository));
         });
 
-        updater.check();
-
-        get.restore();
+        updater.checkForUpdates();
+        assert(get.called);
     });
 
     it('should check empty response array from github releases', function () {
-        var get = sinon.stub(requestStub, 'get', function (url, callback) {
+        var get = sandbox.stub(requestStub, 'get', function (url, callback) {
             callback(false, {}, "[]");
         });
 
-        updater.check(function (error) {
-            assert.equal(error, 'No releases have been made!');
+        updater.on('error', function (error) {
+            assert.equal(error.message, 'No releases have been made!');
         });
 
-        get.restore();
+        updater.checkForUpdates();
+        assert(get.called);
     });
 
     it('should check for invalid tag name from github releases', function () {
-        var get = sinon.stub(requestStub, 'get', function (url, callback) {
+        var get = sandbox.stub(requestStub, 'get', function (url, callback) {
             callback(false, {}, JSON.stringify([{tag_name: 'a.b.c'}]));
         });
 
-        updater.check(function (error) {
-            assert.equal(error, 'The latest release in github has an invalid tag version!');
+        updater.on('error', function (error) {
+            assert.equal(error.message, 'The latest release in github has an invalid tag version!');
         });
 
-        get.restore();
+        updater.checkForUpdates();
+        assert(get.called);
     });
 
     it('should find no later version', function () {
-        var get = sinon.stub(requestStub, 'get', function (url, callback) {
+        var get = sandbox.stub(requestStub, 'get', function (url, callback) {
             callback(false, {}, JSON.stringify([{tag_name: '0.0.8'}]));
         });
 
-        updater.check(function (error, ver) {
-            assert(!ver);
-        });
+        let spy = sinon.spy();
+        updater.on('updates-none', spy);
 
-        get.restore();
+        updater.checkForUpdates();
+        assert(get.called);
+        assert(spy.called);
     });
 
     it('should find no assets', function () {
-        var get = sinon.stub(requestStub, 'get', function (url, callback) {
+        var get = sandbox.stub(requestStub, 'get', function (url, callback) {
             callback(false, {}, JSON.stringify([{tag_name: '0.1.0'}]));
         });
 
-        updater.check(function (error) {
-            assert.equal(error, 'The latest version has no assets!');
+        updater.on('error', function (error) {
+            assert.equal(error.message, 'The latest version has no assets!');
         });
 
-        get.restore();
+        updater.checkForUpdates();
+        assert(get.called);
     });
 
     it('should find no asset that ends with .exe', function () {
-        var get = sinon.stub(requestStub, 'get', function (url, callback) {
+        var get = sandbox.stub(requestStub, 'get', function (url, callback) {
             callback(false, {}, JSON.stringify([{tag_name: '0.1.0', assets: []}]));
         });
 
-        updater.check(function (error) {
-            assert.equal(error, 'The latest version has no asset that ends with ".exe"');
+        updater.on('error', function (error) {
+            assert.equal(error.message, 'The latest version has no asset that ends with ".exe"');
         });
 
-        get.restore();
+        updater.checkForUpdates();
+        assert(get.called);
     });
 
     it('should find update with no error', function () {
-        var get = sinon.stub(requestStub, 'get', function (url, callback) {
+        var get = sandbox.stub(requestStub, 'get', function (url, callback) {
             callback(false, {}, JSON.stringify([ { tag_name: '0.1.0', assets: [{name: "blabla.exe", browser_download_url: 'thesource'}] } ]));
         });
 
-        updater.check(function (error) {
-            assert(!error);
-            assert.equal(updater.update.version, '0.1.0' );
-            assert.equal(updater.update.source, 'thesource');
+        updater.on('updates-found', function (update) {
+            assert.equal(update.version, '0.1.0' );
+            assert.equal(update.source, 'thesource');
         });
 
-        get.restore();
+        updater.checkForUpdates();
+        assert(get.called);
     });
 
 });
